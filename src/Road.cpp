481298e796bfaed044bf0c764bf08b1a1bf76c06
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2019-03-17 17:24:21 -0400

    Initialized Road class

---
+++ src/Road.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2019 Maitreya Venkataswamy - All Rights Reserved
+ */
+
+#include "Road.h"
+
+Road::Road() {
+    // TODO: Implement Road constructor
+}
---
+++ src/Road.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2019 Maitreya Venkataswamy - All Rights Reserved
+ */
+
+#ifndef CA_TRAFFIC_SIMULATION_ROAD_H
+#define CA_TRAFFIC_SIMULATION_ROAD_H
+
+
+class Road {
+public:
+    Road();
+};
+
+
+#endif //CA_TRAFFIC_SIMULATION_ROAD_H
