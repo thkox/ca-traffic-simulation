70befd2013f35d5f640dd6a170012cbde583c1bd
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2019-03-17 19:28:04 -0400

    Added Simulation object initialization

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,19 @@
 
 #include <iostream>
 
+#include "Simulation.h"
+
 int main() {
+#ifndef DEBUG
+    srand(time(NULL));
+#endif
+
+    // Create a Simulation object for the current simulation
+    Simulation* simulation_ptr = new Simulation();
+
+    // Delete the Simulation object
+    delete simulation_ptr;
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
