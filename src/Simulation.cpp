a90cd733657fcff5744b6d08f80f832bf7e0df52
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2019-03-17 18:21:51 -0400

    Initialized simulation class

---
+++ src/Simulation.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2019 Maitreya Venkataswamy - All Rights Reserved
+ */
+
+#include "Simulation.h"
+
+Simulation::Simulation() {
+    // TODO: Implement constructor
+}
---
+++ src/Simulation.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2019 Maitreya Venkataswamy - All Rights Reserved
+ */
+
+#ifndef CA_TRAFFIC_SIMULATION_SIMULATION_H
+#define CA_TRAFFIC_SIMULATION_SIMULATION_H
+
+
+class Simulation {
+private:
+public:
+    Simulation();
+};
+
+
+#endif //CA_TRAFFIC_SIMULATION_SIMULATION_H
