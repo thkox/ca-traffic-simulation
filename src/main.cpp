2a4ef5d6d645e2922267f678ef6de0766401c351
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-12-31 16:27:44 +0200

    run_simulation only in rank==0

--- src/main.cpp
+++ src/main.cpp
@@ -25,45 +25,53 @@ int main(int argc, char** argv) {
     srand(time(NULL));
 #endif
 
-    // Create an Inputs object to contain the simulation parameters
-    Inputs inputs = Inputs();
-    if (inputs.loadFromFile() != 0) {
-        return 1;
-    }
-
-    std::ofstream log_file("debug_log.txt", std::ios::app);
-
-    if (!log_file.is_open()) {
-        std::cerr << "Failed to open log file" << std::endl;
-        return 1;
-    }
-
-
-    // Create a Simulation object for the current simulation
-    Simulation* simulation_ptr = new Simulation(inputs);
 
     // Initialize MPI
     int rank, size;
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-
+    std::ofstream log_file("debug_log.txt", std::ios::app);
+    if (!log_file.is_open()) {
+        std::cerr << "Failed to open log file" << std::endl;
+        MPI_Finalize();
+        return 1;
+    }
 
     log_file << "Rank " << rank << ": " << "I am rank " << rank << " of " << size << std::endl;
 
+    // Create an Inputs object to contain the simulation parameters
+    Inputs inputs = Inputs();
+    if (rank == 0) {
+        if (inputs.loadFromFile() != 0) {
+            log_file.close();
+            MPI_Finalize();
+            return 1;
+        }
+    }
+
+    // Broadcast the inputs to all processes
+    MPI_Bcast(&inputs, sizeof(Inputs), MPI_BYTE, 0, MPI_COMM_WORLD);
+
+    // Create a Simulation object for the current simulation only in the master process
+    Simulation* simulation_ptr = nullptr;
+    if (rank == 0) {
+        simulation_ptr = new Simulation(inputs);
+    }
 
     // Run the Simulation
-    simulation_ptr->run_simulation(rank, size, log_file);
+    if (simulation_ptr != nullptr) {
+        simulation_ptr->run_simulation(rank, size, log_file);
+    }
 
+    // Delete the Simulation object only in the master process
+    if (simulation_ptr != nullptr) {
+        delete simulation_ptr;
+    }
 
     // MPI Finalize
     MPI_Finalize();
-
-    // Delete the Simulation object
-    delete simulation_ptr;
-
-
 
     // Close the log file
     log_file.close();
